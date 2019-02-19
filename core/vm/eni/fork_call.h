86fca146a5a6cb6e92d97e524d91eedfeb41e5b6
mingc <mingc@skymizer.com>
2019-02-19 15:36:06 +0800

    fix warnings

--- core/vm/eni/fork_call.h
+++ core/vm/eni/fork_call.h
@@ -173,8 +173,8 @@ eni_return_data fork_call(
                 *status = ENI_FAILURE;
                 fprintf(stderr, "ENI_FAILURE AAA %d AAA\n", __LINE__);
                 fprintf(stderr, "signal = %s\n", strsignal(sig));
-                fprintf(stderr, "strlen(child_exe_result) = %d\n", strlen(child_exe_result));
-                fprintf(stderr, "child_exe_result = %s\n", child_exe_result);
+                fprintf(stderr, "get_result_len(child_exe_result) = %zu\n", (get_result_len(child_exe_result)));
+                fprintf(stderr, "child_exe_result = %s\n", (char*)child_exe_result);
                 // fprintf(stderr, "regard it as normal termination anyway.\n");
                 // *status = ENI_SUCCESS;
                 // return child_exe_result;

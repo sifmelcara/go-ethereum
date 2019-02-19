1d061bd6bcd3165f21201b2140395c4b80f61f71
mingc <mingc@skymizer.com>
2019-02-19 23:55:13 +0800

    0.05 -> 0.001

--- core/vm/eni/fork_call.h
+++ core/vm/eni/fork_call.h
@@ -328,7 +328,7 @@ eni_return_data wait_and_read_from_child(int pid, int pfd, int* eni_status) {
         else if (nread == 0) { // EOF
             struct timespec tim;
             tim.tv_sec  = 0;
-            tim.tv_nsec = 50000000L; // 0.05 second
+            tim.tv_nsec = 1000000L; // 1ms
             nanosleep(&tim, NULL);
 
             goto end;
